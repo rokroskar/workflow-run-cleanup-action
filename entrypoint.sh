f9b38bb6bc80ab89c8dbf13c305e4969f560714c
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2020-01-31 00:21:53 +0100

    rename script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,47 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
+set -x
+# simple script to cancel a github actions workflow given its name
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+if [ -z "$GITHUB_WORKFLOW" ]
+then
+  echo "Must specify GITHUB_WORKFLOW"
+  exit 1
+fi
+
+if [ -z "$GITHUB_TOKEN" ]
+then
+  echo "Must specify GITHUB_TOKEN"
+  exit 1
+fi
+
+if [ -z "$GITHUB_REPOSITORY" ]
+then
+  echo "Must specify GITHUB_REPOSITORY"
+  exit 1
+fi
+
+# jq queries
+
+jq_workflow_id=".workflows |.[]| select(.name==\"${GITHUB_WORKFLOW}\") .id"
+jq_run_id=".workflow_runs | .[] | select(.head_branch==\"${GITHUB_REF:11}\" and .status==\"in_progress\") | .id"
+
+# get the github workflow ID
+
+GITHUB_API=https://api.github.com
+
+auth_header="Authorization: token ${GITHUB_TOKEN}"
+
+workflow_id=$(curl -s ${GITHUB_API}/repos/${GITHUB_REPOSITORY}/actions/workflows -H ${auth_header} | jq "${jq_workflow_id}")
+
+echo "workflow id: "$workflow_id
+
+# get the run id
+run_ids=$(curl -s ${GITHUB_API}/repos/${GITHUB_REPOSITORY}/actions/workflows/${workflow_id}/runs -H ${auth_header} | jq "${jq_run_id}")
+
+echo "run ids: "$run_ids
+
+# cancel the previous runs
+for run_id in $run_ids
+do
+  curl -s -X POST -H "${auth_header}" ${GITHUB_API}/repos/${GITHUB_REPOSITORY}/actions/runs/${run_id}/cancel
+done
